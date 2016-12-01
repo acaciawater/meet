e1c5e0def3229ab79869728a5ddb00fe45efd18f
theo <tkleinen@gmail.com>
2016-12-01 15:15:30 +0100

    home page with google map

--- meet/urls.py
+++ meet/urls.py
@@ -7,12 +7,13 @@ from django.conf.urls import url, include
 from api import MetingResource, UserResource
 from django.contrib import admin
 from tastypie.api import Api
-
+from views import home
 v1 = Api(api_name='v1')
 v1.register(MetingResource())
 v1.register(UserResource())
 
 urlpatterns = [
+    url(r'^$', home),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1.urls)),
     ]
--- meet/views.py
+++ meet/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
+from models import Meting
+import json
 
 # Create your views here.
+def home(request):
+    pts = [{'id':m.pk,'lat':m.latitude,'lon': m.longitude} for m in Meting.objects.all()]
+    return render(request, 'home.html', context = {'content': json.dumps(pts), 'api_key': settings.GOOGLE_MAPS_API_KEY})
