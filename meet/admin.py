c818ca9754abf2fac922ffdefba36dcafd46b3e5
theo <tkleinen@gmail.com>
2016-12-06 16:14:13 +0100

    Adds list display and list filter to MetingAdmin class

--- meet/admin.py
+++ meet/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from models import Meting
 
+class MetingAdmin(admin.ModelAdmin):
+    list_display = ('pk','sensor','date','entity','value','unit')
+    list_filter = ('entity','date','sensor')
+    
 # Register your models here.
-admin.site.register(Meting)
+admin.site.register(Meting,MetingAdmin)
