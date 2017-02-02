76fe503df3c6993ab1b3acd880bcf3293ec965c4
smoquet <sr_moquette@yahoo.com>
2017-02-02 11:05:19 +0100

    adds filter options to map and views

--- meet/views.py
+++ meet/views.py
@@ -5,6 +5,11 @@ from django.conf import settings
 from pip._vendor import requests
 from models import Meting, User, Meetpunt
 import json
+from django.db.models import Q
+import datetime as dt
+
+def epoch_to_datetime(seconds):
+    return dt.datetime.utcfromtimestamp(seconds).strftime("%Y-%m-%d %H:%M:%S.%f%Z")
 
 def count_measurements(user):
     return Meting.objects.filter(sensor_pk__user__id=int(user.id)).count()
@@ -29,14 +34,43 @@ def getpoints(request):
 
 def getseries(request):
     req = request.GET.urlencode()
-    if req: 
-        user_id = req.split('=')[1]
-        mp = Meetpunt.objects.filter(meting__sensor_pk__user__id=user_id)
-        measurements = serializers.serialize('json',mp)
-    if not req:
-        mp = Meetpunt.objects.all()
-        measurements = serializers.serialize('json',mp.distinct())     
-    return HttpResponse(measurements, content_type='application/json')
+    thumbnail_urls = {}
+
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+    user = request.GET.get('user')
+     
+    if start:
+        start = epoch_to_datetime(float(start)/1000)
+                
+    if end:
+        end = epoch_to_datetime(float(end)/1000)
+                   
+    if start!=None and user==None:
+        # all points time-filtererd
+        mps = Meetpunt.objects.exclude(Q(end__lte=start)|Q(start__gte=end))
+    elif start==None and user!=None:
+        # all points of user
+        mps = Meetpunt.objects.filter(meting__sensor_pk__user__id=user)
+    elif start!=None and user!=None:
+        # user points filtered
+        mps = Meetpunt.objects.filter(meting__sensor_pk__user__id=3).exclude(Q(end__lte=start)|Q(start__gte=end))
+    else:
+        # all points         
+        mps = Meetpunt.objects.all()
+    measurements = serializers.serialize('json',mps.distinct())
+    
+    for mp in mps:
+        url = mp.thumbnail.url
+        meting = mp.meting_set.all()[0]
+        entity = meting.entity
+        unit = meting.unit
+        thumbnail_urls[mp.id] = {'url': url, 'entity':entity, 'unit':unit} 
+    thumbnail_urls = json.dumps(thumbnail_urls)
+
+    result = '['+measurements+','+thumbnail_urls+']'     
+    return HttpResponse(result, content_type='application/json')
+    
 
 def getusers(request):
     req = request.GET.urlencode()
@@ -57,5 +91,8 @@ def getusers(request):
     
     return HttpResponse(result, content_type='application/json')
     
+# def getthumbnail(request):
+#     req = request.GET.urlencode()
+#     req = request.path.split('<')[0]
     
     
